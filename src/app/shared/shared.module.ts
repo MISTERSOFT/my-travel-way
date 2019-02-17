ea8776126ae7b5d4048d6d024ee0d381f2ac91b8
MISTEROSFT <sofiane.hamadi.pro@gmail.com>
2019-02-17 23:45:58 +0100

    update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
+import { FileUploaderComponent } from './file-uploader';
+import { TruncatePipe } from './pipes';
 
 @NgModule({
   declarations: [
+    FileUploaderComponent,
+    TruncatePipe,
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
+    FileUploaderComponent,
   ]
 })
 export class SharedModule { }
